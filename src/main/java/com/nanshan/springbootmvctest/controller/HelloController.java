17632394a69489e0abd729b7231d12d1bd3ef9a8
Roger Lo <sobadrush@icloud.com>
2024-10-22 10:35:01 +0800

    [feature] 補全 @RequestMapping

--- src/main/java/com/nanshan/springbootmvctest/controller/HelloController.java
+++ src/main/java/com/nanshan/springbootmvctest/controller/HelloController.java
@@ -3,12 +3,13 @@ package com.nanshan.springbootmvctest.controller;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("/")
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @RequestMapping(path = "/hello", method = RequestMethod.GET)
     public String helloWorld(HttpServletRequest req) {
         System.out.println("~~ Hello world ~~~");
         req.setAttribute("message", "Hello SpringBoot JSP!");
